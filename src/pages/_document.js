5b584606f254001a12e69cd66c812c34b1922cf8
dhruvalk <kothari.dhruval@gmail.com>
2023-11-14 01:25:48 +0800

    make default text black

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html
       lang="en"
-      className="scroll-smooth"
+      className="scroll-smooth text-black"
       style={{ scrollBehavior: "smooth" }}
     >
       <Head />
